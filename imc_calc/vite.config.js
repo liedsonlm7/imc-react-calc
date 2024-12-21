147dc59f688fd9d88338720243d35078b1bb0444
liedsonlm7 <liedsonlm7@gmail.com>
2024-12-21 16:05:37 -0300

    react IMC Calculator

---
+++ imc_calc/vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './', // Certifique-se de definir a base correta se necessário
+});
+
